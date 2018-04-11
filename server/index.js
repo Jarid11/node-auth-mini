140a9d6660f5d34def48b68c864b57ecf8cad9c2
Jarid11 <Marinos1995@gmail.com>
2018-04-11 13:01:24 -0500

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+
+const strategy = require(`${__dirname}/strategy`);
+
+const app = express();
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxage: 10000
+    }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  return done(null, user);
+});
+
+app.get(
+  "/auth",
+  passport.authenticate("auth0", {
+    successRedirect: "/me",
+    failureRedirect: "/auth",
+    failureFlash: true
+  })
+);
+
+app.get("/me", (req, res, next) => {
+  if (!req.user) {
+    res.status(401).json({ message: "Unauthorized" });
+  } else {
+    res.status(200).json(req.user);
+  }
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ server/strategy.js
@@ -0,0 +1,16 @@
+const Auth0Strategy = require("passport-auth0");
+
+module.exports = new Auth0Strategy(
+  {
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    scope: "openid profile",
+    callbackURL: "/auth"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+  }
+);
+
+//done() is like next()
